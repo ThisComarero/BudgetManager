a06c26b596313da5ff9a9fbfdd898819156c35db
Konrad Wójcik <kw.konradwojcik@gmail.com>
2018-11-20 19:22:53 +0100

    wgranie projektu

---
+++ app.js
@@ -0,0 +1,54 @@
+var budgetController = (function () {
+    
+})();
+
+var UIController = (function () {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add_description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings: function() {
+           return DOMstrings; 
+        }
+    }
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+    var setUpEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn ).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
+        var input = UIController.getInput();
+    }
+
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setUpEventListeners();
+        }
+    }
+    
+})(budgetController, UIController);
+
+controller.init();
